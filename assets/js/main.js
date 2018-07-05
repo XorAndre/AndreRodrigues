0cc9870859e19290024f2054e779d211f76fe663
XorAndre <xor2017@outlook.com>
2018-07-05 10:30:33 -0300

    menu responsivo pronto

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,5 @@ $('a').click(function() {
     $doc.animate({ scrollTop: $( $.attr(this, 'href') ).offset().top}, 500);
     return false;
 });
+//Toggle
+$(document).ready(function(){$("#click-menu").click(function(){$("#mostrar").toggle();});});
