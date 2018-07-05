2752d7e2745ee23eaa52e13cd8e34733c36ec4b6
XorAndre <xor2017@outlook.com>
2018-07-05 12:49:35 -0300

    primeiro commit da tarde da index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,22 @@
 //Scrolling
 let $doc = $('html, body');
-$('a').click(function() {
-    $doc.animate({ scrollTop: $( $.attr(this, 'href') ).offset().top}, 500);
+$('a').click(function(){$doc.animate({ 
+    scrollTop: $( $.attr(this, 'href') ).offset().top}, 500);
     return false;
 });
-//Toggle
-$(document).ready(function(){$("#click-menu").click(function(){$("#mostrar").toggle();});});
+//Toggle-menu
+$(document).ready(function(){
+    $("#click-menu").click(function(){
+        $("#mostrar").toggle();
+    });
+});
+//Toggle-config
+$(document).ready(function(){
+    $("#escolha").click(function(){
+        $("#revela-escolha").toggle();
+    });
+});
+//Cores config
+$("#escuro").click(function(){
+    $("#fundo-escuro").addClass("escuro");
+});
